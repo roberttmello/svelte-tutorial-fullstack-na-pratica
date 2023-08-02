f1dbce1cc28f34ae278e1be6e1191ccf2214cf4d
Carlos Robert <crobertdmelo@gmail.com>
2023-08-02 00:07:52 -0300

    Add - Loop added to Navbar

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -1,15 +1,19 @@
 <script>
   import Logo from "./Logo.svelte";
+  const navLinks = [
+    { href:'/', label:'home' },
+    { href:'/about', label:'about' },
+    { href:'/projects',label:'projects' },
+    { href:'/contact',label:'contact' }
+  ]
 </script>
 
 <a href="/">
   <Logo />
 </a>
 
-<a href="/" class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">home</a>
+{#each navLinks as {href, label}}
+  <a {href} class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">{label}</a>
+{/each}
 
-<a href="/about" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">about</a>
-
-<a href="/projects" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">projects</a>
-
-<a href="/contact" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">contact</a>
+<!-- <a href="/" class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">home</a> -->
